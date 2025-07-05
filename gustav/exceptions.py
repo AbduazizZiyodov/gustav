6f412dd3e4ee3ba0c818baabdc8dde8166535fce
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2025-07-06 01:58:13 +0500

    refactor: exceptions

--- gustav/exceptions.py
+++ gustav/exceptions.py
@@ -2,7 +2,7 @@ import typing as t
 
 from gustav.token import Token
 
-__all__ = "GusParseError", "GusRuntimeError"
+__all__ = "GusParseError", "GusRuntimeError", "GusReturn"
 
 
 class GusParseError(BaseException):
@@ -10,8 +10,7 @@ class GusParseError(BaseException):
 
 
 class GusRuntimeError(RuntimeError):
-    token: Token
-    error_message: str
+    __slots__ = ("token", "error_message")
 
     def __init__(self, token: Token, message: str) -> None:
         self.token = token
@@ -22,5 +21,4 @@ class GusReturn(RuntimeError):
     __slots__ = ("value",)
 
     def __init__(self, value: t.Any, *args: t.Any) -> None:
-        super().__init__(*args)
         self.value = value
