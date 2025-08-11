f884b8c1684f419a9ef9cfac6508802a7959e5f3
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2025-08-12 02:44:21 +0500

    chore: setup minimal c-project, cmake & clang format etc.

--- gustav/ast/__init__.py
+++
@@ -1,4 +0,0 @@
-from .statement import Statement, StatementVisitor
-from .expression import Expression, ExpressionVisitor
-
-__all__ = ("Expression", "Statement", "ExpressionVisitor", "StatementVisitor")
