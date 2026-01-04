9e9779ff1ded5972cde77bb459170ae8c1e295f2
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-01-04 15:11:08 +0500

    feat: vm implementation, core lifecycle

---
+++ include/vm.h
@@ -0,0 +1,30 @@
+#ifndef GUSTAV_VM_H
+#define GUSTAV_VM_H
+
+#include "common.h"
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 512
+
+typedef struct {
+	Chunk *chunk;
+	uint8_t *ip;
+	Value stack[STACK_MAX];
+	Value *stack_top;
+} VM;
+
+typedef enum {
+	INTERPRET_OK,
+	INTERPRET_COMPILE_ERROR,
+	INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void init_vm(void);
+void free_vm(void);
+InterpretResult interpret(Chunk *chunk);
+
+void push(Value value);
+Value pop(void);
+
+#endif
