7e27de9beb1ac460463b1980421aea0eb1da72ec
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-01-11 04:50:08 +0500

    refactor: format CMakeLists.txt, add ERANGE check, define UNREACHABLE explicitly on debug mode

--- include/common.h
+++ include/common.h
@@ -5,6 +5,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef DEBUG
+#define UNREACHABLE()                                                        \
+	do {                                                                 \
+		fprintf(stderr,                                              \
+			"[%s:%d] This code should not be reached in %s()\n", \
+			__FILE__, __LINE__, __func__);                       \
+		abort();                                                     \
+	} while (false)
+#else
 #define UNREACHABLE()
+#endif
 
 #endif
