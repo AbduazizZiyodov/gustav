89b77b090ed40c622253473c078cfb907d3bb643
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-01-18 17:04:03 +0500

    fix: set pointer to NULL after realloc, cast to void

--- include/memory.h
+++ include/memory.h
@@ -1,6 +1,5 @@
 #ifndef GUSTAV_MEMORY_H
 #define GUSTAV_MEMORY_H
-
 #include <stddef.h>
 
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (size_t)((capacity) * 2))
@@ -12,10 +11,18 @@
 	(type *)reallocate(pointer, sizeof(type) * (size_t)(old_count), \
 			   sizeof(type) * (size_t)(new_count))
 
-#define FREE_ARRAY(type, pointer, old_count) \
-	reallocate(pointer, sizeof(type) * (size_t)(old_count), 0)
+#define FREE_ARRAY(type, pointer, old_count)                                  \
+	do {                                                                  \
+		(void)reallocate(pointer, sizeof(type) * (size_t)(old_count), \
+				 0);                                          \
+		(pointer) = NULL;                                             \
+	} while (false)
 
-#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+#define FREE(type, pointer)                                 \
+	do {                                                \
+		(void)reallocate(pointer, sizeof(type), 0); \
+		(pointer) = NULL;                           \
+	} while (false)
 
 void *reallocate(void *pointer, size_t old_size, size_t new_size);
 void free_objects(void);
