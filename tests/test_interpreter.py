c00071cb3b2d152257ba1ca5b3da1fea9546cd16
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-01-31 13:02:16 +0500

    test: add old tests

---
+++ tests/test_interpreter.py
@@ -0,0 +1,26 @@
+import pytest
+
+from pathlib import Path
+
+from .utils import extract_expected, run_file, collect_tests, make_relative
+
+
+interpreter_tests = collect_tests()
+
+
+@pytest.mark.parametrize(
+    "file_path",
+    interpreter_tests,
+    ids=make_relative(test_files=interpreter_tests),
+)  # type: ignore[misc,unused-ignore]
+def test_interpreter(file_path: Path) -> None:
+    expected = extract_expected(file_path)
+
+    # recursion error test case
+    if "stack_overflow.gus" in file_path.as_posix():
+        actual = run_file(file_path, output_as_str=True)
+        assert "RecursionError" in actual
+        return
+
+    actual = run_file(file_path)
+    assert actual == expected, f"{file_path} failed"
---
+++ tests/utils.py
@@ -0,0 +1,59 @@
+import sys
+import subprocess
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from gustav.gustav import Scanner
+
+GUS_EXEC = f"{sys.executable} -m gustav"
+MARKER = "// expect: "
+
+
+def extract_expected(file_path: Path) -> list[str]:
+    expected: list[str] = []
+
+    with file_path.open() as file:
+        for line in file:
+            if MARKER in line:
+                _, _, part = line.partition(MARKER)
+                part = part.replace("\n", "")
+
+                expected.append(part)
+
+    return expected
+
+
+def run_file(file_path: Path, output_as_str: bool = False) -> str | list[str]:
+    args = [*GUS_EXEC.split(), str(file_path)]
+    proc = subprocess.run(args, capture_output=True, text=True)
+
+    output = proc.stdout + proc.stderr
+
+    return output.splitlines() if not output_as_str else output
+
+
+def run_scanner(source: str) -> list[str]:
+    return list(map(lambda token: token.__test_repr__(), Scanner(source).get_tokens()))
+
+
+def make_relative(test_files: list[Path]) -> list[str]:
+    return list(
+        map(
+            lambda file: file.relative_to(Path(__file__).parent).as_posix(),
+            test_files,
+        )
+    )
+
+
+def collect_tests(test_scanner: bool = False) -> list[Path]:
+    root = Path(__file__).parent / "scenarios"
+
+    return sorted(
+        filter(
+            lambda file: "scanning" in file.parts
+            if test_scanner
+            else ("scanning" not in file.parts) and ("benchmark" not in file.parts),
+            root.rglob("*.gus"),
+        )
+    )
