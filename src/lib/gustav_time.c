913cc02263ee8eefa5ccd5e293ca10a62234cba8
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-02-28 13:21:36 +0500

    feat: separate time library under lib folder, implement monotonic time functions (sleep, clock)

---
+++ src/lib/gustav_time.c
@@ -0,0 +1,81 @@
+/* NOLINTNEXTLINE(bugprone-reserved-identifier) */
+#define _POSIX_C_SOURCE 200809L
+
+#include <errno.h>
+/* NOLINTNEXTLINE(misc-include-cleaner) */
+#include <time.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#include "native_functions.h"
+#include "value.h"
+
+value_t clock_native(size_t arg_count __attribute__((unused)),
+		     value_t *args __attribute__((unused)))
+{
+	struct timespec ts;
+	/* NOLINTNEXTLINE(misc-include-cleaner) */
+	if (clock_gettime(CLOCK_MONOTONIC, &ts) != 0) {
+		return NIL_VAL;
+	}
+
+	double seconds = (double)ts.tv_sec + ((double)ts.tv_nsec / 1e9);
+	return NUMBER_VAL(seconds);
+}
+
+value_t gustav_sleep(size_t arg_count __attribute__((unused)),
+		     value_t *args __attribute__((unused)))
+{
+	// TODO(abduaziz): better error handling, <0, no arg ...
+	if (arg_count < 1) {
+		return NIL_VAL;
+	}
+
+	double seconds = AS_NUMBER(args[0]);
+
+	if (!(seconds > 0.0)) {
+		return NIL_VAL;
+	}
+
+	struct timespec now;
+	if (clock_gettime(CLOCK_MONOTONIC, &now) != 0) {
+		return NIL_VAL;
+	}
+
+	long add_sec = (long)seconds;
+	double frac = seconds - (double)add_sec;
+
+	if (frac < 0) {
+		add_sec -= 1;
+		frac += 1.0;
+	}
+
+	struct timespec target;
+	target.tv_sec = now.tv_sec + add_sec;
+	target.tv_nsec = now.tv_nsec + (long)(frac * 1e9);
+
+	if (target.tv_nsec >= 1000000000L) {
+		target.tv_sec += target.tv_nsec / 1000000000L;
+		target.tv_nsec = target.tv_nsec % 1000000000L;
+	}
+
+	while (1) {
+	/* NOLINTNEXTLINE(misc-include-cleaner) */
+		int r = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &target,
+					NULL);
+		if (r == 0) {
+			break;
+		}
+
+		if (r == EINTR) {
+			continue;
+		}
+
+		errno = r;
+		break;
+	}
+
+	return NIL_VAL;
+}
--- src/native_functions.c
+++ src/native_functions.c
@@ -1,22 +1,8 @@
-#include <time.h>
-#include <unistd.h>
-
 #include "native_functions.h"
-#include "value.h"
 
-value_t clock_native(size_t arg_count __attribute__((unused)),
-		     value_t *args __attribute__((unused)))
-{
-	return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
-}
-
-value_t gustav_sleep(size_t arg_count __attribute__((unused)),
-		     value_t *args __attribute__((unused)))
-{
-	/* NOLINTNEXTLINE(concurrency-mt-unsafe) */
-	sleep((unsigned int)AS_NUMBER(args[0]));
-	return NIL_VAL;
-}
-
-const NativeFunctionPair NATIVE_FUNCTIONS[2] = { { clock_native, "clock" },
-						 { gustav_sleep, "sleep" } };
+const NativeFunctionPair NATIVE_FUNCTIONS[2] = {
+	//< clang-format off
+	{ clock_native, "clock" },
+	{ gustav_sleep, "sleep" }
+	//< clang-format on
+};
