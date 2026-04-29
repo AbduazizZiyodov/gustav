80dd8a5da8a60d7263f2132cee04cab0fa370c79
Abduaziz Ziyodov <abduaziz.ziyodov@mail.ru>
2026-04-29 08:54:25 +0500

    build: add steps to produce valgrind builds

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-
 clear
-
 readonly PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$PROJECT_ROOT"
 
@@ -11,17 +9,49 @@ find src include -name '*.[ch]' | xargs clang-format --verbose -i
 
 mkdir -p target
 
-for build_type in Debug Release; do
-    build_dir="build-${build_type,,}"
-    echo -e "\n== [build $build_type in $build_dir] =="
+# deeebug
+echo -e "\n== [build debug in build-debug] =="
+cmake -B build-debug --fresh \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DSANITIZE=ON \
+      -DDEBUG_STRESS_GC=ON \
+      -DDEBUG_LOG_GC=ON \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build-debug --parallel "$(nproc)"
+cp build-debug/gustav target/gustav_debug
+echo -e "== [/build debug in build-debug] ==\n"
 
-    cmake -B "$build_dir" --fresh \
-          -DCMAKE_BUILD_TYPE="$build_type" \
-          -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# regular release build
+echo -e "\n== [build release in build-release] =="
+cmake -B build-release --fresh \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build-release --parallel "$(nproc)"
+cp build-release/gustav target/gustav_release
+echo -e "== [/build release in build-release] ==\n"
 
-    cmake --build "$build_dir" --parallel "$(nproc)"
-    cp "$build_dir/gustav" "target/gustav_${build_type,,}"
-    echo -e "== [/build $build_type in $build_dir] ==\n"
-done
+# valgrind with no sanitizers & no GC stress
+echo -e "\n== [build valgrind in build-valgrind] =="
+cmake -B build-valgrind --fresh \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DSANITIZE=OFF \
+      -DDEBUG_STRESS_GC=OFF \
+      -DDEBUG_LOG_GC=OFF \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build-valgrind --parallel "$(nproc)"
+cp build-valgrind/gustav target/gustav_valgrind
+echo -e "== [/build valgrind in build-valgrind] ==\n"
+
+# valgrind with GC stress
+echo -e "\n== [build valgrind via GC stress in build-valgrind-stress] =="
+cmake -B build-valgrind-stress --fresh \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DSANITIZE=OFF \
+      -DDEBUG_STRESS_GC=ON \
+      -DDEBUG_LOG_GC=OFF \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build-valgrind-stress --parallel "$(nproc)"
+cp build-valgrind-stress/gustav target/gustav_valgrind_stress
+echo -e "== [/build valgrind via GC stress in build-valgrind-stress] ==\n"
 
 ln -sf "build-debug/compile_commands.json" compile_commands.json
