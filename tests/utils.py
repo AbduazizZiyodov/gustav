7dcd325e1bee12ae1a5bf50a4d158944b9876225
Abduaziz Ziyodov <mail@ziyodov.uz>
2026-08-03 00:06:38 +0500

    fix(test):  use proper capture fixture, fix 3 test cases

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,8 @@ import subprocess
 import sys
 from pathlib import Path
 
+from pytest import CaptureFixture
+
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 GUS_EXEC = "./target/gustav_release"
@@ -12,7 +14,7 @@ EXCLUDED_SCENARIOS: tuple[str, ...] = (
     "loop_statement.gus",
     "break_continue_errors.gus",
     "break_continue.gus",
-    "ternary.gus"
+    "ternary.gus",
 )
 
 
@@ -24,36 +26,31 @@ def extract_expected(file_path: Path) -> list[str]:
             if MARKER in line:
                 _, _, part = line.partition(MARKER)
                 part = part.replace("\n", "")
-
                 expected.append(part)
 
     return expected
 
 
-def run_file(file_path: Path, output_as_str: bool = False) -> str | list[str]:
+def run_file(capfd: CaptureFixture, file_path: Path) -> list[str]:
     args = [*GUS_EXEC.split(), str(file_path)]
-    proc = subprocess.run(args, capture_output=True, text=True)
+    subprocess.run(args)  # noqa: PLW1510
 
-    output = proc.stdout + proc.stderr
+    captured = capfd.readouterr()
+    output = captured.out + captured.err
 
     result = output.splitlines()
 
-    if result[0].startswith("Gustav v"):
+    if result and result[0].startswith("Gustav v"):
         result.pop(0)
 
-    if result[0] == "":
+    if result and result[0] == "":
         result.pop(0)
 
     return result
 
 
 def make_relative(test_files: list[Path]) -> list[str]:
-    return list(
-        map(
-            lambda file: file.relative_to(Path(__file__).parent).as_posix(),
-            test_files,
-        )
-    )
+    return [file.relative_to(Path(__file__).parent).as_posix() for file in test_files]
 
 
 def collect_tests() -> list[Path]:
@@ -61,9 +58,11 @@ def collect_tests() -> list[Path]:
 
     return sorted(
         filter(
-            lambda file: file.name not in EXCLUDED_SCENARIOS
-            and "benchmark" not in file.parts
-            and "scanning" not in file.parts,
+            lambda file: (
+                file.name not in EXCLUDED_SCENARIOS
+                and "benchmark" not in file.parts
+                and "scanning" not in file.parts
+            ),
             root.rglob("*.gus"),
         )
     )
