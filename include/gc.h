c8e645f2783d82e124f3bd6d59b097efe5d381d1
Abduaziz Ziyodov <mail@ziyodov.uz>
2026-08-03 22:05:00 +0500

    feat: define separate/explicit GC struct

--- include/gc.h
+++ include/gc.h
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <stddef.h>
+
 #include "value.h"
 
 #define GC_HEAP_GROW_FACTOR 2
 
+typedef struct {
+	Object *objects;
+	size_t bytes_allocated;
+	size_t next_gc;
+
+	size_t gray_count;
+	size_t gray_capacity;
+	Object **gray_stack;
+} GC;
+
+extern GC gc;
+
+void GC_Init(void);
 void GC_Collect(void);
 
 void GC_MarkObject(Object *object);
