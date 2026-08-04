7882795aabfa5768242269aab53081727d1a8cc0
Abduaziz Ziyodov <mail@ziyodov.uz>
2026-08-04 16:09:34 +0500

    docs: init

---
+++ docs/source/conf.py
@@ -0,0 +1,11 @@
+project = "Gustav"
+copyright = "2026, Abduaziz"
+author = "Abduaziz"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "furo"
+html_static_path = ["_static"]
